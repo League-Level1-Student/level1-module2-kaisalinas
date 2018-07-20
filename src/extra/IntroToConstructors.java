4819115b98b1c1308cefd1d97c651ac72f482622
league <league@imac-7.attlocal.net>
2018-07-19 19:55:45 -0700

    Lots of things

---
+++ src/extra/IntroToConstructors.java
@@ -0,0 +1,10 @@
+package extra;
+
+public class IntroToConstructors {
+public static void main(String[] args) {
+	Platypus platty = new Platypus("platty");
+	platty.sayHi();
+	Platypus patty = new Platypus("patty");
+	patty.sayHi();
+}
+}
